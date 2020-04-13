ec58f1c726280ca5a15091d7b0a962b262de96c3
sumanmondal4 <sumaninsearch@gmail.com>
2020-04-13 12:36:02 +0530

    chekin login feature as of now

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
+import { AuthGuard } from '../app/helpers/auth.guard';
 import { PageNotFoundComponent } from './components/common/page-not-found.component';
 import { HomeComponent } from './components/common/home.component';
+import { LoginComponent } from './components/users/login.component';
 import { RegisterComponent } from './components/users/register.component';
 import { DashboardComponent } from './components/users/dashboard.component';
 import { MapsComponent } from './components/users/maps.component';
 import { ProfileComponent } from './components/users/profile.component';
 
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'maps', component: MapsComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'maps', component: MapsComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
@@ -23,6 +28,9 @@ const appRoutes: Routes = [
   declarations: [],
   imports: [
     CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(
       appRoutes
     )
--- web/src/app/components/users/login.component.ts
+++ web/src/app/components/users/login.component.ts
@@ -1,74 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
-import { UserService } from '../../services/user.service';
+
 import { AuthenticationService } from '../../services/authentication.service';
 import { AlertService } from '../../services/alert.service';
-@Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+
+@Component({ 
+    selector: 'app-login',
+    templateUrl: 'login.component.html' 
 })
-export class RegisterComponent implements OnInit {
-  title = 'Covid 19 Sample Project';
-  registerForm: FormGroup;
+export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
     loading = false;
     submitted = false;
-
+    returnUrl: string;
+    
     constructor(
         private formBuilder: FormBuilder,
         private route: ActivatedRoute,
         private router: Router,
-        private userService: UserService,
         private authenticationService: AuthenticationService,
-
         private alertService: AlertService
     ) {
         // redirect to home if already logged in
-          if (this.authenticationService.currentUserValue) {
+        if (this.authenticationService.currentUserValue) {
             this.router.navigate(['/']);
-          }
+        }
     }
 
     ngOnInit() {
-        this.registerForm = this.formBuilder.group({
-            firstName: ['', Validators.required],
-            lastName: ['', Validators.required],
+        this.loginForm = this.formBuilder.group({
             email: ['', Validators.required],
-            phone: ['', [Validators.required, Validators.minLength(10)]],
-            password: ['', [Validators.required, Validators.minLength(6)]]
+            password: ['', Validators.required]
         });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
     // convenience getter for easy access to form fields
-    get f() { return this.registerForm.controls; }
+    get f() { return this.loginForm.controls; }
 
     onSubmit() {
         this.submitted = true;
 
         // reset alerts on submit
-         this.alertService.clear();
+        this.alertService.clear();
 
         // stop here if form is invalid
-        if (this.registerForm.invalid) {
+        if (this.loginForm.invalid) {
             return;
         }
 
         this.loading = true;
-        console.log(this.registerForm.value);
-        this.userService.register(this.registerForm.value)
+        this.authenticationService.login(this.f.email.value, this.f.password.value)
             .pipe(first())
             .subscribe(
                 data => {
-                  console.log(data);
-                   this.alertService.success('Registration successful', true);
-                   this.router.navigate(['/login']);
+                    this.router.navigate([this.returnUrl]);
                 },
                 error => {
                     this.alertService.error(error);
                     this.loading = false;
                 });
     }
-
 }
